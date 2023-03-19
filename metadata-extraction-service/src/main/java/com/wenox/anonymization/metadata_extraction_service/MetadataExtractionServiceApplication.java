ea1b93c293c62f1775492775df2b0fb1d5c7637b
Szymon Pluta <wenoxar@gmail.com>
2023-03-19 15:23:27 +0100

    feat: metadata-service

---
+++ metadata-extraction-service/src/main/java/com/wenox/anonymization/metadata_extraction_service/MetadataExtractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wenox.anonymization.metadata_extraction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataExtractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataExtractionServiceApplication.class, args);
+	}
+
+}
---
+++ metadata-extraction-service/src/test/java/com/wenox/anonymization/metadata_extraction_service/MetadataExtractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenox.anonymization.metadata_extraction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataExtractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
