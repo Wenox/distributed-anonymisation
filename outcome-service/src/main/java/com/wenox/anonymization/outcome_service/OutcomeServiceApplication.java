d18172538ada7b99b84fe82e94a2f8cc8feb0bb5
Szymon Pluta <wenoxar@gmail.com>
2023-04-09 12:26:08 +0200

    feat: outcome-service template

---
+++ outcome-service/src/main/java/com/wenox/anonymization/outcome_service/OutcomeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wenox.anonymization.outcome_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutcomeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutcomeServiceApplication.class, args);
+    }
+}
