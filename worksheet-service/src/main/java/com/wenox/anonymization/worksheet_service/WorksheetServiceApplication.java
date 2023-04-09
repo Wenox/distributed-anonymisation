0f8525214b8023d7318e6a7f18b809724e791000
Szymon Pluta <wenoxar@gmail.com>
2023-04-09 12:15:23 +0200

    feat: worksheet-service template

---
+++ worksheet-service/src/main/java/com/wenox/anonymization/worksheet_service/WorksheetServiceApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.wenox.anonymization.worksheet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksheetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetadataExtractionServiceApplication.class, args);
+    }
+}
