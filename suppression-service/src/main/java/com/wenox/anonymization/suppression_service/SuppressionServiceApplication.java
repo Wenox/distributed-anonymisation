9604d9b3bf2a4e84edd4c4f7c1213a9c5bb774d6
Szymon Pluta <wenoxar@gmail.com>
2023-04-17 23:24:20 +0200

    feat: suppression-service template

---
+++ suppression-service/src/main/java/com/wenox/anonymization/suppression_service/SuppressionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wenox.anonymization.suppression_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppressionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppressionServiceApplication.class, args);
+    }
+}
