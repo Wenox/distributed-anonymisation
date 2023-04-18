879f41fc39d7937e003d9fb151bb3741471e0d5e
Szymon Pluta <wenoxar@gmail.com>
2023-04-19 00:29:54 +0200

    feat: redis

--- suppression-service/src/main/java/com/wenox/anonymization/suppression_service/PersonController.java
+++ suppression-service/src/main/java/com/wenox/anonymization/suppression_service/PersonController.java
@@ -1,12 +1,12 @@
 package com.wenox.anonymization.suppression_service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
 import java.util.UUID;
 
@@ -16,16 +16,18 @@ import java.util.UUID;
 public class PersonController {
 
     private final PersonRepository personRepository;
+    private final Jedis jedis;
 
-    public PersonController(PersonRepository personRepository) {
+    public PersonController(PersonRepository personRepository, Jedis jedis) {
         this.personRepository = personRepository;
+        this.jedis = jedis;
     }
 
     @PostMapping
-    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) throws JsonProcessingException {
         person.setId(UUID.randomUUID());
-        log.info("saving person: {}", person);
         Person savedPerson = personRepository.save(person);
+        jedis.set(person.getId().toString(), new ObjectMapper().writeValueAsString(person));
         log.info("saved person: {}", person);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
     }
---
+++ suppression-service/src/main/java/com/wenox/anonymization/suppression_service/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.wenox.anonymization.suppression_service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public Jedis jedis() {
+        return new Jedis(redisHost, redisPort);
+    }
+}
