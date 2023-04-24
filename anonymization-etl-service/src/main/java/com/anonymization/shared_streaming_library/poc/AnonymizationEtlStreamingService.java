4efdc558b45197e162ccd4fc35e60826254244fd
Szymon Pluta <wenoxar@gmail.com>
2023-04-24 23:00:06 +0200

    feat: circuit breaker

--- anonymization-etl-service/src/main/java/com/anonymization/shared_streaming_library/poc/AnonymizationEtlStreamingService.java
+++ anonymization-etl-service/src/main/java/com/anonymization/shared_streaming_library/poc/AnonymizationEtlStreamingService.java
@@ -3,11 +3,13 @@ package com.anonymization.shared_streaming_library.poc;
 import com.anonymization.shared_streaming_library.*;
 import com.anonymization.shared_streaming_library.sink.StreamingSink;
 import com.anonymization.shared_streaming_library.source.StreamingSource;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
-import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,7 +19,6 @@ import scala.Tuple2;
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -29,7 +30,10 @@ public class AnonymizationEtlStreamingService implements EtlStreamingService, Se
 
     private final StreamingSource streamingSource;
     private final StreamingSink streamingSink;
+
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
     private final RetryTemplate retryTemplate;
+
     private final ExtractService extractService;
     private final TransformService transformService;
     private final Column2ScriptService column2ScriptService;
@@ -43,14 +47,10 @@ public class AnonymizationEtlStreamingService implements EtlStreamingService, Se
     }
 
     private void startEtlStreamingQuery() {
-        try {
-            retryTemplate.execute(retryContext -> {
-                processEtlStreaming();
-                return null;
-            });
-        } catch (Exception ex) {
-            log.error("Error occurred during ETL processing", ex);
-        }
+        retryTemplate.execute(retryContext -> {
+            processEtlStreaming();
+            return null;
+        });
     }
 
     @Scheduled(fixedDelayString = "${streaming.restartInterval:60000}")
@@ -64,36 +64,47 @@ public class AnonymizationEtlStreamingService implements EtlStreamingService, Se
         }
     }
 
-    public void processEtlStreaming() throws TimeoutException, StreamingQueryException {
-        // Step 1: Read the Kafka stream
-        Dataset<AnonymizationTask> inputDF = streamingSource.fetchTasks();
+    public void processEtlStreaming() {
+        CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("etlStreamingCircuitBreaker");
 
-        // Step 2: Extract
-        Dataset<Tuple2<Column2, AnonymizationTask>> extractedTuple = inputDF.map(
-                (MapFunction<AnonymizationTask, Tuple2<Column2, AnonymizationTask>>) extractService::extract,
-                Encoders.tuple(Encoders.bean(Column2.class), Encoders.bean(AnonymizationTask.class))
-        );
+        Try<Void> result = Try.run(() -> {
 
-        // Step 3: Transform - Anonymization
-        Dataset<Tuple2<Column2, AnonymizationTask>> anonymizedTuple = extractedTuple.map(
-                (MapFunction<Tuple2<Column2, AnonymizationTask>, Tuple2<Column2, AnonymizationTask>>) transformService::anonymize,
-                Encoders.tuple(Encoders.bean(Column2.class), Encoders.bean(AnonymizationTask.class))
-        );
+            // Step 1: Read the Kafka stream
+            Dataset<AnonymizationTask> inputDF = streamingSource.fetchTasks();
 
-        // Step 4: Transform – SQL script
-        Dataset<Tuple2<Column2Script, AnonymizationTask>> scriptTuple = anonymizedTuple.map(
-                (MapFunction<Tuple2<Column2, AnonymizationTask>, Tuple2<Column2Script, AnonymizationTask>>) column2ScriptService::create,
-                Encoders.tuple(Encoders.bean(Column2Script.class), Encoders.bean(AnonymizationTask.class))
-        );
+            // Step 2: Extract
+            Dataset<Tuple2<Column2, AnonymizationTask>> extractedTuple = inputDF.map(
+                    (MapFunction<AnonymizationTask, Tuple2<Column2, AnonymizationTask>>) extractService::extract,
+                    Encoders.tuple(Encoders.bean(Column2.class), Encoders.bean(AnonymizationTask.class))
+            );
 
-        // Step 5: Load
-        Dataset<SuccessEvent> successEvents = scriptTuple.map(
-                (MapFunction<Tuple2<Column2Script, AnonymizationTask>, SuccessEvent>) loadService::load,
-                Encoders.bean(SuccessEvent.class)
-        );
+            // Step 3: Transform - Anonymization
+            Dataset<Tuple2<Column2, AnonymizationTask>> anonymizedTuple = extractedTuple.map(
+                    (MapFunction<Tuple2<Column2, AnonymizationTask>, Tuple2<Column2, AnonymizationTask>>) transformService::anonymize,
+                    Encoders.tuple(Encoders.bean(Column2.class), Encoders.bean(AnonymizationTask.class))
+            );
 
-        // Step 6: Sink
-        streamingSink.sink(successEvents);
+            // Step 4: Transform – SQL script
+            Dataset<Tuple2<Column2Script, AnonymizationTask>> scriptTuple = anonymizedTuple.map(
+                    (MapFunction<Tuple2<Column2, AnonymizationTask>, Tuple2<Column2Script, AnonymizationTask>>) column2ScriptService::create,
+                    Encoders.tuple(Encoders.bean(Column2Script.class), Encoders.bean(AnonymizationTask.class))
+            );
+
+            // Step 5: Load
+            Dataset<SuccessEvent> successEvents = scriptTuple.map(
+                    (MapFunction<Tuple2<Column2Script, AnonymizationTask>, SuccessEvent>) loadService::load,
+                    Encoders.bean(SuccessEvent.class)
+            );
+
+            // Step 6: Sink
+            streamingSink.sink(successEvents);
+
+        }).recover(throwable -> {
+            log.error("Error occurred during ETL processing", throwable);
+            return null;
+        });
+
+        circuitBreaker.executeSupplier(() -> result);
     }
 }
 
