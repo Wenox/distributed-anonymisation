1ee1f9025d4d70d536b5304d41d98a3e0a22545c
Szymon Pluta <wenoxar@gmail.com>
2023-12-17 21:06:17 +0100

    feat: simplify: single mongoDB server, with database per service encapuslation

---
+++ mongo-init.js
@@ -0,0 +1,24 @@
+function createDbAndUser(dbName, user, pwd) {
+
+    var createdDb = db.getSiblingDB(dbName);
+
+    createdDb.createCollection('initCollection');
+
+    createdDb.createUser({
+        user: user,
+        pwd: pwd,
+        roles: [{ role: 'readWrite', db: dbName }]
+    });
+}
+
+
+console.log("Initialising Mongodb databases and users...")
+
+createDbAndUser('BLUEPRINTS_DB', 'BLUEPRINTS_USER', 'BLUEPRINTS_PASSWORD');
+createDbAndUser('RESTORATIONS_DB', 'RESTORATIONS_USER', 'RESTORATIONS_PASSWORD');
+createDbAndUser('METADATA_DB', 'METADATA_USER', 'METADATA_PASSWORD');
+createDbAndUser('WORKSHEETS_DB', 'WORKSHEETS_DB', 'WORKSHEETS_PASSWORD');
+createDbAndUser('ANONYMISATION_EXECUTION_DB', 'ANONYMISATION_EXECUTION_USER', 'ANONYMISATION_EXECUTION_PASSWORD');
+createDbAndUser('ANONYMISATION_SAGA_DB', 'ANONYMISATION_SAGA_USER', 'ANONYMISATION_SAGA_PASSWORD');
+
+console.log("Successfully initialised all Mongodb databases and users.")
