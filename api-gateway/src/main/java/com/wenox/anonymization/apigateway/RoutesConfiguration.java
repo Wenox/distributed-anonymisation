a8991c428cb655969ce4d55ea1fcb2c3b7cf607c
Szymon Pluta <wenoxar@gmail.com>
2023-12-17 18:36:46 +0100

    feat: api gateway for importing workflow

---
+++ api-gateway/src/main/java/com/wenox/anonymization/apigateway/RoutesConfiguration.java
@@ -0,0 +1,9 @@
+package com.wenox.anonymization.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfiguration {
+
+}
