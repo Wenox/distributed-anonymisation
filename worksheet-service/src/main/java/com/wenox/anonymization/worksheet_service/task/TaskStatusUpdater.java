a38a6cca10423c7f98c1032239d74c487fd14a38
Szymon Pluta <wenoxar@gmail.com>
2024-01-09 02:49:09 +0100

    refactor: task status updater

--- worksheet-service/src/main/java/com/wenox/anonymization/worksheet_service/task/TaskStatusUpdater.java
+++ worksheet-service/src/main/java/com/wenox/anonymization/worksheet_service/task/TaskStatusUpdater.java
@@ -5,88 +5,123 @@ import com.wenox.anonymization.worksheet_service.exception.TaskNotFoundException
 import com.wenox.anonymization.worksheet_service.operation.Operation;
 import com.wenox.anonymization.worksheet_service.operation.OperationRepository;
 import com.wenox.anonymization.worksheet_service.operation.TaskStatus;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
-@Service
-@RequiredArgsConstructor
+@Component
 public class TaskStatusUpdater {
 
-    private final OperationRepository operationRepository;
+    private final OperationRepository repository;
+    private final Map<TaskStatus, StatusUpdateStrategy> updateStrategies;
 
-    private final Set<TaskStatus> statusesPostTransformedAnonymization = Set.of(TaskStatus.TRANSFORMED_ANONYMIZATION, TaskStatus.TRANSFORMED_SQL_SCRIPT, TaskStatus.FINISHED);
-    private final Set<TaskStatus> statusesPostTransformedSqlScript = Set.of(TaskStatus.TRANSFORMED_SQL_SCRIPT, TaskStatus.FINISHED);
+    public TaskStatusUpdater(OperationRepository repository, List<StatusUpdateStrategy> updateStrategies) {
+        this.repository = repository;
+        this.updateStrategies = updateStrategies
+                .stream()
+                .collect(Collectors.toMap(StatusUpdateStrategy::getApplicableStatus, Function.identity()));
+    }
 
     @KafkaListener(topics = KafkaConstants.TOPIC_EXTRACTION_SUCCESS, groupId = "blueprint-service-group")
-    public void onExtracted(String taskId) {
-        Operation operation = operationRepository.findByTaskId(taskId)
-                .orElseThrow(() -> new TaskNotFoundException("Task not found with taskId: " + taskId));
-
-        TaskStatus existingStatus = operation.getStatus();
-        if (existingStatus != TaskStatus.STARTED) {
-            log.info("Ignoring task update to {} – already updated to {} – taskId: {}", TaskStatus.EXTRACTED, existingStatus, taskId);
-            return;
-        }
-
-        operation.setStatus(TaskStatus.EXTRACTED);
-        operationRepository.save(operation);
-
-        log.info("Updated task from {} to {} – taskId: {}", existingStatus, TaskStatus.EXTRACTED, taskId);
+    void onExtracted(String taskId) {
+        updateStatus(taskId, TaskStatus.EXTRACTED);
     }
 
     @KafkaListener(topics = KafkaConstants.TOPIC_TRANSFORMATION_ANONYMIZE_SUCCESS, groupId = "blueprint-service-group")
-    public void onTransformedAnonymization(String taskId) {
-        Operation operation = operationRepository.findByTaskId(taskId)
-                .orElseThrow(() -> new TaskNotFoundException("Task not found with taskId: " + taskId));
-
-        TaskStatus existingStatus = operation.getStatus();
-        if (statusesPostTransformedAnonymization.contains(existingStatus)) {
-            log.info("Ignoring task update to {} – already updated to {} – taskId: {}", TaskStatus.TRANSFORMED_ANONYMIZATION, existingStatus, taskId);
-            return;
-        }
-
-        operation.setStatus(TaskStatus.TRANSFORMED_ANONYMIZATION);
-        operationRepository.save(operation);
-
-        log.info("Updated task from {} to {} – taskId: {}", existingStatus, TaskStatus.TRANSFORMED_ANONYMIZATION, taskId);
+    void onTransformedAnonymization(String taskId) {
+        updateStatus(taskId, TaskStatus.TRANSFORMED_ANONYMIZATION);
     }
 
     @KafkaListener(topics = KafkaConstants.TOPIC_TRANSFORMATION_SCRIPT_SUCCESS, groupId = "blueprint-service-group")
-    public void onTransformedSqlScript(String taskId) {
-        Operation operation = operationRepository.findByTaskId(taskId)
-                .orElseThrow(() -> new TaskNotFoundException("Task not found with taskId: " + taskId));
-
-        TaskStatus existingStatus = operation.getStatus();
-        if (statusesPostTransformedSqlScript.contains(existingStatus)) {
-            log.info("Ignoring task update to {} – already updated to {} – taskId: {}", TaskStatus.TRANSFORMED_SQL_SCRIPT, existingStatus, taskId);
-            return;
-        }
-
-        operation.setStatus(TaskStatus.TRANSFORMED_SQL_SCRIPT);
-        operationRepository.save(operation);
-
-        log.info("Updated task from {} to {} – taskId: {}", existingStatus, TaskStatus.TRANSFORMED_SQL_SCRIPT, taskId);
+    void onTransformedSqlScript(String taskId) {
+        updateStatus(taskId, TaskStatus.TRANSFORMED_SQL_SCRIPT);
     }
 
     @KafkaListener(topics = KafkaConstants.TOPIC_LOAD_SUCCESS, groupId = "blueprint-service-group")
-    public void onLoaded(String taskId) {
-        Operation operation = operationRepository.findByTaskId(taskId)
+    void onLoaded(String taskId) {
+        updateStatus(taskId, TaskStatus.FINISHED);
+    }
+
+    private void updateStatus(String taskId, TaskStatus newStatus) {
+        Operation operation = repository.findByTaskId(taskId)
                 .orElseThrow(() -> new TaskNotFoundException("Task not found with taskId: " + taskId));
 
-        TaskStatus existingStatus = operation.getStatus();
-        if (existingStatus == TaskStatus.FINISHED) {
-            log.info("Ignoring task update to {} – already updated to {} – taskId: {}", TaskStatus.FINISHED, existingStatus, taskId);
-            return;
+        StatusUpdateStrategy strategy = updateStrategies.get(newStatus);
+        if (strategy.canUpdateStatus(operation)) {
+            operation.setStatus(newStatus);
+            repository.save(operation);
+            log.info("Updated task from {} to {} – taskId: {}", operation.getStatus(), newStatus, taskId);
+        } else {
+            log.info("Ignoring task update to {} – already updated to {} – taskId: {}", newStatus, operation.getStatus(), taskId);
         }
-
-        operation.setStatus(TaskStatus.FINISHED);
-        operationRepository.save(operation);
-
-        log.info("Updated task from {} to {} – taskId: {}", existingStatus, TaskStatus.FINISHED, taskId);
     }
 }
+
+interface StatusUpdateStrategy {
+    boolean canUpdateStatus(Operation operation);
+    TaskStatus getApplicableStatus();
+}
+
+@Component
+class ExtractedStatusUpdateStrategy implements StatusUpdateStrategy {
+    @Override
+    public boolean canUpdateStatus(Operation operation) {
+        return operation.getStatus() == TaskStatus.STARTED;
+    }
+
+    @Override
+    public TaskStatus getApplicableStatus() {
+        return TaskStatus.EXTRACTED;
+    }
+}
+
+@Component
+class TransformedAnonymizationStatusUpdateStrategy implements StatusUpdateStrategy {
+    private final Set<TaskStatus> postTransformedAnonymization = Set.of(TaskStatus.TRANSFORMED_ANONYMIZATION, TaskStatus.TRANSFORMED_SQL_SCRIPT, TaskStatus.FINISHED);
+
+    @Override
+    public boolean canUpdateStatus(Operation operation) {
+        return !postTransformedAnonymization.contains(operation.getStatus());
+    }
+
+    @Override
+    public TaskStatus getApplicableStatus() {
+        return TaskStatus.TRANSFORMED_ANONYMIZATION;
+    }
+}
+
+@Component
+class TransformedSqlScriptStatusUpdateStrategy implements StatusUpdateStrategy {
+    private final Set<TaskStatus> postTransformedSqlScript = Set.of(TaskStatus.TRANSFORMED_SQL_SCRIPT, TaskStatus.FINISHED);
+
+    @Override
+    public boolean canUpdateStatus(Operation operation) {
+        return !postTransformedSqlScript.contains(operation.getStatus());
+    }
+
+    @Override
+    public TaskStatus getApplicableStatus() {
+        return TaskStatus.TRANSFORMED_SQL_SCRIPT;
+    }
+}
+
+@Component
+class FinishedStatusUpdateStrategy implements StatusUpdateStrategy {
+    @Override
+    public boolean canUpdateStatus(Operation operation) {
+        return operation.getStatus() != TaskStatus.FINISHED;
+    }
+
+    @Override
+    public TaskStatus getApplicableStatus() {
+        return TaskStatus.FINISHED;
+    }
+}
+
