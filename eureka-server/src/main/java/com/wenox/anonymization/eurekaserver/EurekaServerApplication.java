d5615bf52de7a80af9e44b2a6c095e93e7d2160e
Szymon Pluta <wenoxar@gmail.com>
2024-01-09 05:01:57 +0100

    refactor: rename to eureka-server

--- eureka-server/src/main/java/com/wenox/anonymization/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/wenox/anonymization/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.wenox.anonymization.eurekaservice;
+package com.wenox.anonymization.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
