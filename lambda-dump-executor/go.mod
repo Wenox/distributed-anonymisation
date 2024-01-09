9e3744672ebf1068fc535234d5ffc13b525227b4
Szymon Pluta <wenoxar@gmail.com>
2024-01-09 04:53:47 +0100

    refactor: delete lambda-dumo-executor

--- lambda-dump-executor/go.mod
+++
@@ -1,8 +0,0 @@
-module com.anonymization/lambda-dump-executor
-
-go 1.20
-
-require (
-	github.com/aws/aws-lambda-go v1.40.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
--- lambda-dump-executor/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo 'Building main.go...'
-GOOS=linux GOARCH=amd64 go build -o main
-
-echo 'Packaging into deployment.zip...'
-zip deployment.zip main
-
-echo 'Done'
