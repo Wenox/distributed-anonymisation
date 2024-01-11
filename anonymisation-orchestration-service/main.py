048cb2b3be804dbb87fcedf1f63cafca7d5e1fa2
Szymon Pluta <wenoxar@gmail.com>
2024-01-11 20:09:40 +0100

    feat: exporting process

--- anonymisation-orchestration-service/main.py
+++ anonymisation-orchestration-service/main.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 
 import boto3
@@ -10,7 +9,7 @@ from retry import retry
 from fragments_population import get_tasks_statuses
 from http_client import async_request_with_circuit_breaker_and_retries
 from mirror import create_mirror
-from saga import Saga, saga_collection, update_saga_status, SagaStatus
+from outcome import Outcome, outcomes_collection, update_outcome_status, OutcomeStatus, update_to_mirror_ready
 from trigger_lambda import router as trigger_lambda_router, INPUT_BUCKET, OUTPUT_BUCKET, LAMBDA_NAME
 from logger_config import setup_logger
 
@@ -24,44 +23,54 @@ class TriggerRequest(BaseModel):
     worksheet_id: str
 
 
-def create_saga(worksheet_id: str):
-    logger.info(f"-----> Step 1: Creating new Saga for worksheet_id: {worksheet_id}...")
-    saga = Saga(worksheet_id)
-    saga_collection.insert_one(saga.to_dict())
-    logger.info(f"<----- Step 1: Created new Saga:\n{json.dumps(saga.to_dict(), indent=4)}")
-    return saga
+def initialize_outcome(worksheet_id: str):
+    logger.info(f"-----> Step 1: Initializing new outcome for worksheet: {worksheet_id}...")
+    outcome = Outcome(worksheet_id)
+    outcomes_collection.insert_one(outcome.to_dict())
+    logger.info(f"<----- Step 1: Initialized new outcome:\n{json.dumps(outcome.to_dict(), indent=4)}")
+    return outcome
 
 
 @task(name="Create mirror")
-def start_create_mirror(saga: Saga):
-    logger.info(f"-----> Step 1: Creating mirror for worksheet_id: {saga.worksheet_id}...")
-    response = create_mirror(saga.worksheet_id)
-    update_saga_status(saga.saga_id, SagaStatus.MIRROR_READY)
-    logger.info(f"<----- Step 1: Created mirror. Updated saga to {SagaStatus.MIRROR_READY}.")
-    return response
+def start_create_mirror(outcome: Outcome):
+    logger.info(f"-----> Step 2: Creating mirror for worksheet: {outcome.worksheet_id}...")
+    response, success = create_mirror(outcome.worksheet_id)
 
-
-@task(name="Check tasks status")
-@retry(tries=5, delay=1.0, max_delay=1.0, backoff=1, jitter=(0, 0), logger=logger)
-def tasks_status(saga: Saga):
-    logger.info(f"-----> Step 2: Checking tasks status for worksheet_id: {saga.worksheet_id}...")
-    response = get_tasks_statuses(saga.worksheet_id)
-
-    if response['allSuccessful']:
-        logger.info("All tasks completed!")
-        update_saga_status(saga.saga_id, SagaStatus.TASKS_COMPLETED)
+    if success:
+        update_to_mirror_ready(outcome, response.get('mirrorId'))
+        logger.info(f"<----- Step 2: Created mirror. Updated outcome to {OutcomeStatus.MIRROR_READY}.")
     else:
-        logger.info("Some tasks not successful...")
-        if is_still_processing_tasks(tasks_by_status=response['tasksByStatus']):
-            logger.info("Still processing some tasks...")
-            # Re-run the task up to 7 times, timeout 3000 ms
-            raise Exception("Still processing some tasks...")
-        else:
-            logger.info("Some tasks were failed.")
-            # Re-run the task up to 4 times, timeout 1000 ms
-            raise Exception("Some tasks were failed, retrying...")
+        update_outcome_status(outcome, OutcomeStatus.MIRROR_FAILED)
+        logger.info(f"<----- Step 2: Failed to create mirror. Updated outcome to {OutcomeStatus.MIRROR_FAILED}.")
 
-    logger.info(f"<----- Step 2: Retrieved fragments. Updated saga to {SagaStatus.TASKS_COMPLETED}")
+    return response, success
+
+
+
+@task(name="Check fragments readiness")
+@retry(tries=5, delay=1.0, max_delay=1.0, backoff=1, jitter=(0, 0), logger=logger)
+def check_fragments_readiness(outcome: Outcome):
+    logger.info(f"-----> Step 3: Checking fragments readiness for worksheet: {outcome.worksheet_id}...")
+    response, success = get_tasks_statuses(outcome.worksheet_id)
+
+    if success:
+        if response['allSuccessful']:
+            logger.info("All anonymisation tasks are completed! Fragments are ready.")
+            update_outcome_status(outcome, OutcomeStatus.FRAGMENTS_READY)
+            logger.info(f"<----- Step 3: Fragments are ready. Updated outcome to {OutcomeStatus.FRAGMENTS_READY}")
+        else:
+            logger.info("Some anonymisation tasks not successful...")
+            if is_still_processing_tasks(tasks_by_status=response['tasksByStatus']):
+                logger.info("Still processing some anonymisation tasks...")
+                raise Exception("Still processing some anonymisation tasks...")
+            else:
+                logger.info("Some anonymisation tasks were failed.")
+                raise Exception("Some anonymisation tasks were failed, retrying...")
+    else:
+        update_outcome_status(outcome, OutcomeStatus.FRAGMENTS_NOT_READY)
+        logger.info(f"<----- Step 3: Fragments are not yet ready. Updated outcome to {OutcomeStatus.FRAGMENTS_NOT_READY}.")
+
+    return response, success
 
 
 def is_still_processing_tasks(tasks_by_status):
@@ -71,90 +80,93 @@ def is_still_processing_tasks(tasks_by_status):
         len(tasks_by_status['TRANSFORMED_SQL_SCRIPT']) > 0
 
 
-@task(name="Merge anonymization fragments")
-def merge_anonymization_fragments(saga: Saga):
-    logger.info(f"-----> Step 3: Merging anonymization fragments worksheet_id: {saga.worksheet_id}...")
+@task(name="Merge anonymisation fragments")
+def merge_anonymisation_fragments(outcome: Outcome):
+    logger.info(f"-----> Step 4: Merging anonymisation fragments for worksheet: {outcome.worksheet_id}...")
     try:
-        # Set up the Lambda client
         lambda_client = boto3.client('lambda', region_name='eu-west-2')
-
-        # Prepare the payload
         payload = {
             "inputBucket": INPUT_BUCKET,
-            "inputDirectory": saga.worksheet_id,
+            "inputDirectory": outcome.worksheet_id,
             "outputBucket": OUTPUT_BUCKET,
             "outputScript": 'result-output.sql',
         }
 
         logger.info(f"==========> AWS Lambda Request:\n{json.dumps(payload, indent=4)}")
-        # Invoke the Lambda function
         lambda_response = lambda_client.invoke(
             FunctionName=LAMBDA_NAME,
             InvocationType='RequestResponse',
             Payload=json.dumps(payload),
         )
-
-        # Read and parse the response payload
         response_payload = json.loads(lambda_response['Payload'].read().decode('utf-8'))
+        logger.info(f"<========== AWS Lambda Response:\n{json.dumps(response_payload, indent=4)}")
 
-        # Log the response
-        response = {"status": "success", "response": response_payload}
-        logger.info(f"<========== AWS Lambda Response:\n{json.dumps(response, indent=4)}")
+        update_outcome_status(outcome, OutcomeStatus.FRAGMENTS_MERGED)
+        logger.info(f"<----- Step 4: Merged anonymisation fragments. Updated outcome to {OutcomeStatus.FRAGMENTS_MERGED}")
+        return response_payload, True
 
     except Exception as e:
         logger.error(f"Error triggering Lambda: {e}")
+        update_outcome_status(outcome, OutcomeStatus.FRAGMENTS_MERGE_FAILED)
+        logger.info(f"<----- Step 4: Merging anonymisation fragments failed. Updated outcome to {OutcomeStatus.FRAGMENTS_MERGE_FAILED}")
+        return None, False
 
-    update_saga_status(saga.saga_id, SagaStatus.MERGE_SUCCESS)
-    logger.info(f"<----- Step 3: Merged anonymization fragments. Updated saga to {SagaStatus.MERGE_SUCCESS}")
-
-
-@task(name="Execute anonymization script")
-def execute_anonymization_script(saga, db_name):
-    logger.info(f"-----> Step 4: Executing anonymization script: {saga.worksheet_id}...")
+@task(name="Execute anonymisation script")
+def execute_anonymisation_script(outcome):
+    logger.info(f"-----> Step 5: Executing anonymisation script for worksheet: {outcome.worksheet_id}...")
     try:
         anonymization_execution_path = "http://localhost:8500/api/v1/execute-anonymization"
-        file_name = f"{saga.worksheet_id}/result-output.sql"
+        file_name = f"{outcome.worksheet_id}/result-output.sql"
         response = async_request_with_circuit_breaker_and_retries("POST", anonymization_execution_path,
-                                                                  json={"db_name": db_name, "file_path": file_name},
+                                                                  json={"mirrorId": outcome.mirror_id, "file_path": file_name},
                                                                   timeout=60)
         return response.json()
     except Exception as e:
         print(f"Request failed: {e}")
 
-    update_saga_status(saga.saga_id, SagaStatus.ANONYMIZATION_EXECUTED)
-    logger.info(f"<----- Step 4: Executed anonymization script: {SagaStatus.ANONYMIZATION_EXECUTED}")
+    update_outcome_status(outcome, OutcomeStatus.SCRIPT_EXECUTED)
+    logger.info(f"<----- Step 5: Executed anonymisation script: {OutcomeStatus.SCRIPT_EXECUTED}")
 
 
-@task(name="Generate anonymization dump")
-def generate_anonymization_dump(saga, db_name):
-    logger.info(f"-----> Step 5: Generating anonymization dump: {saga.worksheet_id}...")
+@task(name="Generate anonymisation dump")
+def generate_anonymisation_dump(outcome):
+    logger.info(f"-----> Step 6: Generating anonymisation dump: {outcome.worksheet_id}...")
     try:
         generate_dump_path = "http://localhost:8500/api/v1/execute-anonymization/generate-dump"
         response = async_request_with_circuit_breaker_and_retries("POST", generate_dump_path,
-                                                                  json={"db_name": db_name},
+                                                                  json={"mirrorId": outcome.mirror_id},
                                                                   timeout=60)
         return response.json()
     except Exception as e:
         print(f"Request failed: {e}")
 
-    update_saga_status(saga.saga_id, SagaStatus.DUMP_GENERATED)
-    logger.info(f"<----- Step 5: Generated anonymization dump: {SagaStatus.DUMP_GENERATED}")
+    update_outcome_status(outcome, OutcomeStatus.DUMP_GENERATED)
+    logger.info(f"<----- Step 6: Generated anonymisation dump: {OutcomeStatus.DUMP_GENERATED}")
 
 
-@flow(name="Anonymization Saga Workflow")
-def anonymization_saga_workflow(saga):
-    response = start_create_mirror(saga)
-    logger.info(f"Returned response {response}")
-    tasks_status(saga)
-    merge_anonymization_fragments(saga)
-    execute_anonymization_script(saga, response['db_name'])
-    generate_anonymization_dump(saga, response['db_name'])
+@flow(name="Exporting Process Saga")
+def exporting_process_saga(outcome):
+    response, success = start_create_mirror(outcome)
+    if not success:
+        logger.info("Finishing exporting saga: mirror creation failed")
+        return
+
+    response, success = check_fragments_readiness(outcome)
+    if not success:
+        logger.info("Finishing exporting saga: some fragments are not ready")
+        return
+
+    response, success = merge_anonymisation_fragments(outcome)
+    if not success:
+        logger.info("Finishing exporting saga: merging anonymisation fragments failed")
+
+    execute_anonymisation_script(outcome)
+    generate_anonymisation_dump(outcome)
 
 
-@app.post("/api/anonymization-sagas")
-async def trigger_anonymization_saga_workflow(body: TriggerRequest, background_tasks: BackgroundTasks):
-    logger.info(
-        f"-----> /api/anonymization-sagas: Started anonymization saga workflow. Body:\n{json.dumps(body.dict(), indent=4)}")
-    saga = create_saga(body.worksheet_id)
-    background_tasks.add_task(anonymization_saga_workflow, saga)
-    return saga.to_dict()
+@app.post("/api/v1/exporting/start")
+async def start_exporting_process(body: TriggerRequest, background_tasks: BackgroundTasks):
+    logger.info(f"-----> /api/v1/exporting/start: Started exporting process for worksheet : {json.dumps(body.dict(), indent=4)}")
+    outcome = initialize_outcome(body.worksheet_id)
+    background_tasks.add_task(exporting_process_saga, outcome)
+    return outcome.to_dict()
--- anonymisation-orchestration-service/outcome.py
+++ anonymisation-orchestration-service/outcome.py
@@ -5,34 +5,52 @@ import pymongo
 from config import MONGODB_CONNECTION_URI
 
 
-class SagaStatus(Enum):
-    CREATED = "CREATED"
+class OutcomeStatus(Enum):
+    INITIALIZED = "INITIALIZED"
     MIRROR_READY = "MIRROR_READY"
-    TASKS_COMPLETED = "TASKS_COMPLETED"
-    MERGE_SUCCESS = "MERGE_SUCCESS"
-    ANONYMIZATION_EXECUTED = "ANONYMIZATION_EXECUTED"
+    FRAGMENTS_READY = "FRAGMENTS_READY"
+    FRAGMENTS_MERGED = "FRAGMENTS_MERGED"
+    SCRIPT_EXECUTED = "SCRIPT_EXECUTED"
     DUMP_GENERATED = "DUMP_GENERATED"
+
+    MIRROR_FAILED = "MIRROR_FAILED"
+    FRAGMENTS_NOT_READY = "FRAGMENTS_NOT_READY"
+    FRAGMENTS_MERGE_FAILED = "FRAGMENTS_MERGE_FAILED"
     FINISHED = "FINISHED"
     FAILED = "FAILED"
 
 
-class Saga:
-    def __init__(self, worksheet_id: str, status: SagaStatus = SagaStatus.CREATED):
-        self.saga_id = uuid4()
+class Outcome:
+    def __init__(self, worksheet_id: str, status: OutcomeStatus = OutcomeStatus.INITIALIZED, mirror_id: str = None):
+        self.outcome_id = uuid4()
         self.status = status
         self.worksheet_id = worksheet_id
+        self.mirror_id = mirror_id
 
     def to_dict(self):
-        return {
-            "saga_id": str(self.saga_id),
+        data = {
+            "outcomeId": str(self.outcome_id),
             "status": self.status.value,
-            "worksheet_id": self.worksheet_id,
+            "worksheetId": self.worksheet_id,
         }
+        if self.mirror_id:
+            data["mirrorId"] = self.mirror_id
+        return data
 
 
-def update_saga_status(saga_id: UUID, status: SagaStatus):
-    saga_collection.update_one(
-        {"saga_id": str(saga_id)}, {"$set": {"status": status.value}}
+def update_outcome_status(outcome: Outcome, status: OutcomeStatus):
+    outcome.status = status
+    outcomes_collection.update_one(
+        {"outcomeId": str(outcome.outcome_id)}, {"$set": {"status": status.value}}
+    )
+
+
+def update_to_mirror_ready(outcome: Outcome, mirror_id: str):
+    outcome.status = OutcomeStatus.MIRROR_READY
+    outcome.mirror_id = mirror_id
+    outcomes_collection.update_one(
+        {"outcomeId": str(outcome.outcome_id)},
+        {"$set": {"status": OutcomeStatus.MIRROR_READY.value, "mirrorId": mirror_id}}
     )
 
 
@@ -42,4 +60,4 @@ client = pymongo.MongoClient(MONGODB_CONNECTION_URI)
 
 # Get the database and collection
 db = client.ANONYMISATION_ORCHESTRATION_DB
-saga_collection = db.saga
+outcomes_collection = db.outcomes
