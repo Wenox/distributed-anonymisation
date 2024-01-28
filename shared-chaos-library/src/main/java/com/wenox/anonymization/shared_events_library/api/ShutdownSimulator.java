a8f7e386436c1c183e554bd3ad2d3a306aaa0c3f
Szymon Pluta <wenoxar@gmail.com>
2024-01-28 18:31:42 +0100

    refactor: ShutdownSimulator

--- shared-chaos-library/src/main/java/com/wenox/anonymization/shared_events_library/api/ShutdownSimulator.java
+++ shared-chaos-library/src/main/java/com/wenox/anonymization/shared_events_library/api/ShutdownSimulator.java
@@ -7,15 +7,17 @@ import java.lang.reflect.Field;
 @Slf4j
 public class ShutdownSimulator {
 
-    public static void crashJVM() {
+    public static void crashJVM(String purpose) {
         try {
             log.error("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             log.error("Crashing JVM to simulate service failure...");
             log.error("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            log.error("Simulation purpose: {}", purpose);
+            log.error("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             Field field = Unsafe.class.getDeclaredField("theUnsafe");
             field.setAccessible(true);
             Unsafe unsafe = (Unsafe) field.get(null);
-            unsafe.putAddress(0, 0); // this will crash JVM
+            unsafe.putAddress(0, 0);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
